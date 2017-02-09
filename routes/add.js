ff7c08f1d12c44d3063d88f3a41ee8d1a6ca964c
Thuy Nguyen <dtn073@ucsd.edu>
2017-02-09 13:41:32 -0800

    lab5 updated

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,21 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+
+
+exports.addFriend = function(req, res) {
+
+	var name = req.query.name;
+
+	var description = req.query.description;
+
+	var image = "http://lorempixel.com/400/400/people";
+
+
+
+	data['friends'].push({"name":name,"description":description,"imageURL":image});
+
+	console.log(data);
+
+	res.render('index', data);
+
+}
